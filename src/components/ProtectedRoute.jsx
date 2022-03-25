2b493438403f31c8891e177f886b696c41adec04
Salman Shaikh <tony903212@gmail.com>
2022-03-25 10:09:15 +0530

    Added prettier config

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,14 +2,8 @@ import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function ProtectedRoute(props) {
-    const { currentUser } = useSelector((state) => state)
+    const { currentUser } = useSelector((state) => state);
     return (
-        <>
-            {currentUser !== null ? (
-                props.children
-            ) : (
-                <Navigate to="/login" />
-            )}
-        </>
+        <>{currentUser !== null ? props.children : <Navigate to="/login" />}</>
     );
 }
